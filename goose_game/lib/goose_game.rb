6831a4a29209d7ec092ec1116b19f525e69e9dde
Roberto Maestroni <r.maestroni@gmail.com>
2018-09-04 18:14:53 +0200

    Initial commit

---
+++ goose_game/lib/goose_game.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "goose_game/version"
+
+module GooseGame
+  # Your code goes here...
+end
---
+++ goose_game/spec/goose_game_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GooseGame do
+  it "has a version number" do
+    expect(GooseGame::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
